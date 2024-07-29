1b56e65dcdf0355ae1ddb9907d2017fa468a43cb
Jaseem Paloth <jaseempalothjp@gmail.com>
2024-07-30 01:08:12 +0530

    feat: Implement solution for the Count Number of Teams problem

---
+++ daily_challenge/count_number_of_teams.py
@@ -0,0 +1,18 @@
+# 1395. Count Number of Teams
+# Topics: Array, Dynamic Programming, Binary Indexed Tree, Segment Tree
+class Solution:
+    def numTeams(self, rating: list[int]) -> int:
+        n = len(rating)
+        if n < 3:
+            return 0
+        dp = [[0, 0] for _ in range(n)]
+        res = 0
+        for i in range(n):
+            for j in range
+                if rating[i] > rating[j]:
+                    dp[i][0] += 1
+                    res += dp[j][0]
+                else:
+                    dp[i][1] += 1
+                    res += dp[j][1]
+        return res
