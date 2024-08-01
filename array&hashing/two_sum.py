60fc95db055245a53993964758272a2cb0b4d0d5
Jaseem Paloth <jaseempalothjp@gmail.com>
2024-08-01 21:40:10 +0530

    feat: Implement two_sum function using hashmap
    This commit adds the implementation of the two_sum function in the Solution class. The function uses a hashmap to store previously encountered numbers and their indices. It iterates through the input list of numbers and checks if the difference between the target and the current number exists in the hashmap. If it does, the function returns the indices of the two numbers that add up to the target. Otherwise, it returns -1. This implementation improves the time complexity of the two_sum function to O(n) by avoiding nested loops.

--- array&hashing/two_sum.py
+++ array&hashing/two_sum.py
@@ -1,6 +1,19 @@
 # 1. Two Sum
 class Solution:
     def two_sum(self, nums: list[int], target: int) -> list[int]:
-        pass
+        prev_map = {}
+
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in prev_map:
+                return [i, prev_map[diff]]
+            prev_map[num] = i
+        return -1
+
+s = Solution()
+nums = [2,7,11,15]
+target = 9
+print(s.two_sum(nums, target))
+
 
 
