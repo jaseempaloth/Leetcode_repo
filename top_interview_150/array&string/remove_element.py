458b6a04bef3f0afb3a52bddcf7865b66abeee55
Jaseem Paloth <jaseempalothjp@gmail.com>
2024-08-03 03:06:17 +0530

    feat: Add dynamic programming solution for the Decode Ways problem

---
+++ top_interview_150/array&string/remove_element.py
@@ -0,0 +1,11 @@
+# 27. Remove Element
+# Topics: Array, Two Pointers
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        return i
+        
