cdc19ba275c318668dd0313f236b870bc03326cf
Jaseem Paloth <jaseempalothjp@gmail.com>
2024-09-04 02:31:30 +0530

    feat: Add solution for the 3 Sum of Digits of String After Convert problem

---
+++ daily_challenge/september/3_sum_of_digits_of_string_after_convert.py
@@ -0,0 +1,21 @@
+class Solution:
+    def get_lucky(self, s: str, k: int) -> int:
+        result = []
+        s = s.lower()
+        for char in s:
+            index = ord(char) - ord('a') + 1
+            result.append(str(index))
+              
+        num_str = ''.join(result)
+        for _ in range(k):
+            total = 0
+            for digit in num_str:
+                total += int(digit)
+            num_str = str(total)
+        return int(num_str)
+            
+
+sln = Solution()
+s = "leetcode"
+k = 2
+print(sln.get_lucky(s, k))
